acfedb8e7d97539d570a423804bc539602009995
Deep Sardana <80479606+deepsardana@users.noreply.github.com>
2024-01-10 15:58:14 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+sudo systemctl stop tomcat
 aws s3 cp s3://clouddeploy-demo/target/LoginWebApp-1.war/sample-java-projects/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /opt/tomcat/webapps/LoginWebApp-1.war
-sudo systemctl restart tomcat
+sudo systemctl starttomcat
